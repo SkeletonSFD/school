3d082c90fdd44a1678a7d71d905d3e04754c49f9
SkeletonSFD <skeletonsfd@gmail.com>
2024-10-23 14:54:51 +0300

    Upgrade

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import requests
 
-places_database =['san francisco',
+
+places=['san francisco',
                'london',
                'svo',
                'череповец'
 ]
 
-parametrs = {'n_Tnqm&lang':'ru'}
+parameters = {'nTnqm':'',
+             'lang':'ru'
+}
 
-for cities_places in places_database:
-    weather_forecast = requests.get(f'https://wttr.in/{cities_places}',params = parametrs)
+for place in places:
+    weather_forecast = requests.get(f'https://wttr.in/{place}',params = parameters)
     weather_forecast.raise_for_status()
     print(weather_forecast.text)
-
- #'https://wttr.in/san%20francisco?n_Tnqm&lang=ru'
- #https://www.youtube.com/watch?v=2AyV7L7ptHQ
